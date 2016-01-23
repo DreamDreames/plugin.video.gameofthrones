be3b31b7eee44b5d6f764669297b2baf0d426ae0
Rainbowbox <twentyfivehours@163.com>
2016-01-23 23:03:07 +0800

    Play videos

--- addon.py
+++ addon.py
@@ -1,21 +1,35 @@
 # -*- coding: utf-8 -*-
 # Game of Thrones from Tencent video
-import xbmcaddon
+import sys
+import urlparse
+import xbmcplugin
 import xbmcgui
 
+plugin_url = sys.argv[0]
+handle = int(sys.arg[1])
+params = dict(urlparse.parse_qsl(sys.argv[2].lstrip('?')))
 
-#with open('vid.txt') as f:
-    #vids = f.readlines()
-    #for idx, vid in enumerate(vids):
-        #print idx, vid.strip('\n')
-        #print 'done'
-addon = xbmcaddon.Addon()
-addonname = addon.getAddonInfo('name')
+__play_url__ = r'http://cache.tv.qq.com/qqplayerout.swf?vid='
 
-line1 = "plugin of"
-line2 = "Game of Thrones"
-line3 = "from tencent video"
+def index():
+    with open('vid.txt') as f:
+        vids = f.readlines()
+        for idx, rawvid in enumerate(vids):
+            vid = rawvid.strip('\n')
+            li = xbmcgui.ListItem('第{}集'.format(idx))
+            url = plugin_url + '?act=play&vid=' + vid
+            xbmcplugin.addDirectoryItem(handle, url, li, True)
 
-xbmcgui.Dialog().ok(addonname, line1, line2, line3)
+    xbmcplugin.endOfDirectory(handle)
+
+def play():
+    link = __play_url__ + params['vid']
+    play_item = xbmcgui.ListItem(path=link)
+    xbmcplugin.setResolvedUrl(handle, True, listitem=play_item)
+
+{
+    'index': index,
+    'play': play
+}[params.get('act', 'index')]()
 
 

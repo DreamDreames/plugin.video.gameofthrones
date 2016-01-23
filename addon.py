fcb7a617c7e0151439a485635b9e1c4b7f1958d1
Rainbowbox <twentyfivehours@163.com>
2016-01-23 21:03:53 +0800

    a dialog hello world

--- addon.py
+++ addon.py
@@ -3,10 +3,19 @@
 import xbmcaddon
 import xmbcgui
 
-with open('vid.txt') as f:
-    vids = f.readlines()
-    for idx, vid in enumerate(vids):
-        print idx, vid.strip('\n')
-        print 'done'
+
+#with open('vid.txt') as f:
+    #vids = f.readlines()
+    #for idx, vid in enumerate(vids):
+        #print idx, vid.strip('\n')
+        #print 'done'
+addon = xmbcaddon.Addon()
+addonname = addon.getAddonInfo('name')
+
+line1 = "plugin of"
+line2 = "Game of Thrones"
+line3 = "from tencent video"
+
+xmbcgui.Dialog().ok(addonname, line1, line2, line3)
 
 

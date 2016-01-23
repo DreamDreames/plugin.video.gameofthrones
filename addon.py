8e4042d4d1ed827245bcb5b237e4e1e84ae78404
Rainbowbox <twentyfivehours@163.com>
2016-01-23 23:35:43 +0800

    Correct module name

--- addon.py
+++ addon.py
@@ -4,7 +4,7 @@ import sys
 import urlparse
 import xbmcplugin
 import xbmcgui
-import resources.vids as vids
+import resources.vids as episode
 
 plugin_url = sys.argv[0]
 handle = int(sys.argv[1])
@@ -13,8 +13,7 @@ params = dict(urlparse.parse_qsl(sys.argv[2].lstrip('?')))
 __play_url__ = r'http://cache.tv.qq.com/qqplayerout.swf?vid='
 
 def index():
-    for idx, rawvid in vids:
-        vid = rawvid.strip('\n')
+    for idx, vid in enumerate(episode.vids):
         li = xbmcgui.ListItem('第{}集'.format(idx))
         url = plugin_url + '?act=play&vid=' + vid
         xbmcplugin.addDirectoryItem(handle, url, li, True)
